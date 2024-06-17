0ea6007b4d5321e6f05ad1c485bdb5c88f857b6d
Luke Bollwerk <lukas8@gmail.com>
2024-06-17 08:19:21 -0600

    fix api call

--- www/request.js
+++ www/request.js
@@ -1,4 +1,10 @@
-function makeRequest(id) {
+function makeRequest(id, eventId) {
     let button = document.getElementById('button-' + id);
     button.disabled = true;
+    fetch('http://localhost:8080/api/v1/event/request/' + eventId + '/' + id, {
+        method: 'POST',
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
 }
