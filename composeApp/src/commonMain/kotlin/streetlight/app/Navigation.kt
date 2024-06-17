3abc29d31320f4a4cea25fbeb449d5c7c9b0da1f
Luke Bollwerk <lukas8@gmail.com>
2024-06-17 12:08:46 -0600

    refactor navigation

--- composeApp/src/commonMain/kotlin/streetlight/app/Navigation.kt
+++ composeApp/src/commonMain/kotlin/streetlight/app/Navigation.kt
@@ -1,11 +1,8 @@
 package streetlight.app
 
 import moe.tlaster.precompose.navigation.BackStackEntry
-import moe.tlaster.precompose.navigation.Navigator
-import moe.tlaster.precompose.navigation.RouteBuilder
 import moe.tlaster.precompose.navigation.path
-import moe.tlaster.precompose.navigation.transition.NavTransition
-import streetlight.app.ui.HomeScreen
+import streetlight.app.ui.DebugScreen
 import streetlight.app.ui.data.AreaEditorScreen
 import streetlight.app.ui.data.AreaListScreen
 import streetlight.app.ui.data.EventEditorScreen
@@ -18,31 +15,108 @@ import streetlight.app.ui.data.RequestEditorScreen
 import streetlight.app.ui.data.RequestListScreen
 import streetlight.app.ui.data.UserEditorScreen
 
-object AppRoutes {
-    const val default = "/home"
-}
+object Scenes {
+    val default = { debug.route }
 
-fun RouteBuilder.appScenes(navigator: Navigator) {
-    // Define a scene to the navigation graph
-    scene(
-        // Scene's route path
-        route = "/home",
-        // Navigation transition for this scene, this is optional
-        navTransition = NavTransition(),
-    ) {
-        HomeScreen(navigator)
+    val debug = AppScene(
+        name = "Debug",
+        route = "/debug",
+    ) { _, navigator ->
+        DebugScreen(navigator)
     }
-    scene("/user/{id}?") { UserEditorScreen(it.getId(), navigator) }
-    scene("/locations") { LocationListScreen(navigator) }
-    scene("/location/{id}?") { LocationEditorScreen(it.getId(), navigator) }
-    scene("/areas") { AreaListScreen(navigator) }
-    scene("/area/{id}?") { AreaEditorScreen(it.getId(), navigator) }
-    scene("/events") { EventListScreen(navigator) }
-    scene("/event/{id}?") { EventEditorScreen(it.getId(), navigator) }
-    scene("/performances") { PerformanceListScreen(navigator) }
-    scene("/performance/{id}?") { PerformanceEditorScreen(it.getId(), navigator) }
-    scene("/requests") { RequestListScreen(navigator) }
-    scene("/request/{id}?") { RequestEditorScreen(it.getId(), navigator) }
+
+    val userEditor = AppScene(
+        name = "Create User",
+        route = "/user/{id}?"
+    ) { bse, navigator ->
+        UserEditorScreen(bse.getId(), navigator)
+    }
+
+    val locationList = AppScene(
+        name = "Locations",
+        route = "/locations"
+    ) { _, navigator ->
+        LocationListScreen(navigator)
+    }
+
+    val locationEditor = AppScene(
+        name = "Create Location",
+        route = "/location/{id}?"
+    ) { bse, navigator ->
+        LocationEditorScreen(bse.getId(), navigator)
+    }
+
+    val areaList = AppScene(
+        name = "Areas",
+        route = "/areas"
+    ) { _, navigator ->
+        AreaListScreen(navigator)
+    }
+
+    val areaEditor = AppScene(
+        name = "Create Area",
+        route = "/area/{id}?"
+    ) { bse, navigator ->
+        AreaEditorScreen(bse.getId(), navigator)
+    }
+
+    val eventList = AppScene(
+        name = "Events",
+        route = "/events"
+    ) { _, navigator ->
+        EventListScreen(navigator)
+    }
+
+    val eventEditor = AppScene(
+        name = "Create Event",
+        route = "/event/{id}?"
+    ) { bse, navigator ->
+        EventEditorScreen(bse.getId(), navigator)
+    }
+
+    val performanceList = AppScene(
+        name = "Performances",
+        route = "/performances"
+    ) { _, navigator ->
+        PerformanceListScreen(navigator)
+    }
+
+    val performanceEditor = AppScene(
+        name = "Create Performance",
+        route = "/performance/{id}?"
+    ) { bse, navigator ->
+        PerformanceEditorScreen(bse.getId(), navigator)
+    }
+
+    val requestList = AppScene(
+        name = "Requests",
+        route = "/requests"
+    ) { _, navigator ->
+        RequestListScreen(navigator)
+    }
+
+    val requestEditor = AppScene(
+        name = "Create Request",
+        route = "/request/{id}?"
+    ) { bse, navigator ->
+        RequestEditorScreen(bse.getId(), navigator)
+    }
 }
 
 fun BackStackEntry.getId() = this.path<Int>("id")
+
+val appScenes = listOf(
+    Scenes.debug,
+    Scenes.userEditor,
+    Scenes.locationList,
+    Scenes.locationEditor,
+    Scenes.areaList,
+    Scenes.areaEditor,
+    Scenes.eventList,
+    Scenes.eventEditor,
+    Scenes.performanceList,
+    Scenes.performanceEditor,
+    Scenes.requestList,
+    Scenes.requestEditor
+)
+
