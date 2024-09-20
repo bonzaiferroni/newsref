36284d59f083f99a6f65bd733698d11169a16e2a
Luke Bollwerk <lukas8@gmail.com>
2024-09-20 13:34:59 -0600

    use postgres

--- model/src/commonMain/kotlin/streetlight/model/core/Area.kt
+++ model/src/commonMain/kotlin/streetlight/model/core/Area.kt
@@ -5,6 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Area(
     override val id: Int = 0,
-    val name: String = "",
     val parentId: Int? = null,
+    val name: String = "",
 ) : IdModel
--- model/src/commonMain/kotlin/streetlight/model/core/Location.kt
+++ model/src/commonMain/kotlin/streetlight/model/core/Location.kt
@@ -9,9 +9,15 @@ data class Location(
     val userId: Int = 0,
     val areaId: Int = 0,
     val name: String = "",
-    val latitude: Double = 0.0,
-    val longitude: Double = 0.0,
-    val address: String = "",
+    val description: String? = null,
+    val address: String? = null,
+    val notes: String? = null,
+    val geoPoint: GeoPoint = GeoPoint(),
     val type: LocationType = LocationType.Unknown,
-    val notes: String = "",
 ) : IdModel
+
+@Serializable
+data class GeoPoint(
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0
+)
