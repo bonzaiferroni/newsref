6a52caaca71a57eaa265695d4e35548daac6c001
Luke Bollwerk <lukas8@gmail.com>
2024-09-24 09:51:27 -0600

    add leafeletmap

--- web/src/jsMain/kotlin/streetlight/web/ui/pages/atlasPage.kt
+++ web/src/jsMain/kotlin/streetlight/web/ui/pages/atlasPage.kt
@@ -6,7 +6,6 @@ import io.kvision.form.text.text
 import io.kvision.html.button
 import io.kvision.html.h3
 import io.kvision.html.p
-import io.kvision.maps.externals.leaflet.map.LeafletMap
 import streetlight.model.dto.UserInfo
 import streetlight.web.Icons
 import streetlight.web.core.AppContext
@@ -19,15 +18,16 @@ fun Container.atlasPage(context: AppContext): PortalEvents? {
     val model = AtlasModel()
     userContext(context) { userInfo ->
         launchedEffect {
-            val map = leafletMap()
+            val map = streetMap()
             model.refresh()
+            map.awaitInflate()
             atlasWidget(context, model, userInfo, map)
         }
     }
     return null
 }
 
-fun Container.atlasWidget(context: AppContext, model: AtlasModel, userInfo: UserInfo, map: LeafletMap) {
+fun Container.atlasWidget(context: AppContext, model: AtlasModel, userInfo: UserInfo, map: StreetMap) {
     col {
         h3("New Location")
         row(alignItems = AlignItems.CENTER) {
