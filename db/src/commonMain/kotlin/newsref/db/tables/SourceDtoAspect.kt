3e1c96ccf255b0e6ff4df52604379a6d1d411bc1
Luke Bollwerk <lukas8@gmail.com>
2025-02-16 11:23:18 -0700

    narrow SourceDto

--- db/src/commonMain/kotlin/newsref/db/tables/SourceDtoAspect.kt
+++ db/src/commonMain/kotlin/newsref/db/tables/SourceDtoAspect.kt
@@ -7,38 +7,28 @@ import org.jetbrains.exposed.sql.ResultRow
 
 object SourceDtoAspect: Aspect(SourceTable) {
     val id = add(SourceTable.id)
-    val hostId = add(SourceTable.hostId)
-    val noteId = add(SourceTable.noteId)
     val url = add(SourceTable.url)
     val title = add(SourceTable.title)
     val type = add(SourceTable.type)
     val score = add(SourceTable.score)
-    val feedPosition = add(SourceTable.feedPosition)
     val imageUrl = add(SourceTable.imageUrl)
     val thumbnail = add(SourceTable.thumbnail)
     val embed = add(SourceTable.embed)
-    val contentCount = add(SourceTable.contentCount)
-    val okResponse = add(SourceTable.okResponse)
+    val wordCount = add(SourceTable.contentCount)
     val seenAt = add(SourceTable.seenAt)
-    val accessedAt = add(SourceTable.accessedAt)
     val publishedAt = add(SourceTable.publishedAt)
 }
 
 internal fun ResultRow.toSourceDto() = SourceDto(
     id = this[SourceDtoAspect.id].value,
-    hostId = this[SourceDtoAspect.hostId].value,
-    noteId = this.getOrNull(SourceDtoAspect.noteId)?.value,
     url = this[SourceDtoAspect.url],
     title = this[SourceDtoAspect.title],
     type = this[SourceDtoAspect.type],
     score = this[SourceDtoAspect.score],
-    feedPosition = this[SourceDtoAspect.feedPosition],
     imageUrl = this[SourceDtoAspect.imageUrl],
     thumbnail = this[SourceDtoAspect.thumbnail],
     embed = this[SourceDtoAspect.embed],
-    contentCount = this[SourceDtoAspect.contentCount],
-    okResponse = this[SourceDtoAspect.okResponse],
+    wordCount = this[SourceDtoAspect.wordCount],
     seenAt = this[SourceDtoAspect.seenAt].toInstantUtc(),
-    accessedAt = this[SourceDtoAspect.accessedAt]?.toInstantUtc(),
     publishedAt = this[SourceDtoAspect.publishedAt]?.toInstantUtc(),
 )
--- model/src/commonMain/kotlin/newsref/model/dto/SourceDto.kt
+++ model/src/commonMain/kotlin/newsref/model/dto/SourceDto.kt
@@ -7,20 +7,15 @@ import newsref.model.core.SourceType
 @Serializable
 data class SourceDto(
     val id: Long = 0,
-    val hostId: Int = 0,
-    val noteId: Long? = null,
     val url: String,
     val title: String? = null,
     val type: SourceType? = null,
     val score: Int? = null,
-    val feedPosition: Int? = null,
     val thumbnail: String? = null,
     val imageUrl: String? = null,
     val embed: String? = null,
-    val contentCount: Int? = null,
-    val okResponse: Boolean,
+    val wordCount: Int? = null,
     val seenAt: Instant,
-    val accessedAt: Instant? = null,
     val publishedAt: Instant? = null,
 ) {
 }
