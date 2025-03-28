20d39c578a9ad876cd622328ca354c400b6c3e16
Luke Bollwerk <lukas8@gmail.com>
2025-03-28 09:06:27 -0600

    style property tile

--- app/src/commonMain/kotlin/newsref/app/blip/controls/PropertyTile.kt
+++ app/src/commonMain/kotlin/newsref/app/blip/controls/PropertyTile.kt
@@ -1,16 +1,26 @@
 package newsref.app.blip.controls
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 import newsref.app.blip.theme.Blip
+import newsref.app.utils.brighten
+import newsref.app.utils.darken
 
 @Composable
 fun PropertyTile(
@@ -20,12 +30,36 @@ fun PropertyTile(
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.width(IntrinsicSize.Max)
+            .shadow(Blip.ruler.shadowElevation)
     ) {
-        Text(title, color = Blip.localColors.contentDim, style = TextStyle(textAlign = TextAlign.Center))
+        Text(
+            text = title,
+            color = Blip.localColors.contentDim,
+            style = TextStyle(textAlign = TextAlign.Center),
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Blip.localColors.surface.darken())
+                .padding(Blip.ruler.innerPadding)
+        )
         Row(
             verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(Blip.ruler.halfSpacing, Alignment.CenterHorizontally),
+            modifier = Modifier.sizeIn(minHeight = 50.dp)
+                .fillMaxWidth()
+                .background(color = Blip.localColors.surface.brighten())
+                .padding(Blip.ruler.innerPadding)
         ) {
             content()
         }
+    }
+}
+
+@Composable
+fun PropertyTile(
+    title: String,
+    content: Any
+) {
+    PropertyTile(title) {
+        Text(content.toString())
     }
 }
--- app/src/commonMain/kotlin/newsref/app/ui/ArticlePropertiesColumn.kt
+++ app/src/commonMain/kotlin/newsref/app/ui/ArticlePropertiesColumn.kt
@@ -2,14 +2,18 @@ package newsref.app.ui
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import newsref.app.blip.controls.*
+import newsref.app.blip.theme.Blip
 import newsref.app.io.*
 import newsref.app.model.*
 import newsref.model.core.HuddleType
 import newsref.model.data.HuddleKey
 import newsref.model.utils.formatSpanLong
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun ArticlePropertiesColumn(
     article: Article,
@@ -20,27 +24,35 @@ fun ArticlePropertiesColumn(
 
     Column {
         H2(article.bestTitle)
-        Text(article.url)
-        article.articleType?.let {
-            PropertyTile("Type") {
-                Text(it.title)
-                if (userState.isLoggedIn) {
-                    HuddleEditorControl(
-                        huddleName = "Edit Article Type",
-                        key = HuddleKey(
-                            pageId = state.article.pageId,
-                            type = HuddleType.EditArticleType
-                        ),
-                    )
+        FlowRow(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Blip.ruler.rowSpaced,
+            verticalArrangement = Blip.ruler.columnSpaced,
+        ) {
+            PropertyTile("url") {
+                Text(article.url)
+            }
+            article.articleType?.let {
+                PropertyTile("Type") {
+                    Text(it.title)
+                    if (userState.isLoggedIn) {
+                        HuddleEditorControl(
+                            huddleName = "Edit Article Type",
+                            key = HuddleKey(
+                                pageId = state.article.pageId,
+                                type = HuddleType.EditArticleType
+                            ),
+                        )
+                    }
                 }
             }
-        }
 
-        article.wordCount?.let { Text("Word count: $it") }
-        article.score?.let { Text("Score: $it") }
-        article.publishedAt?.let { Text("Published: ${it.formatSpanLong()}") }
-            ?: Text("Seen: ${article.seenAt.formatSpanLong()}")
-        article.accessedAt?.let { Text("Accessed: ${it.formatSpanLong()}") }
-            ?: Text("Accessed: never")
+            article.wordCount?.let { PropertyTile("Word Count", it) }
+            article.score?.let { PropertyTile("Score", it) }
+            article.publishedAt?.let { PropertyTile("Published", it.formatSpanLong()) }
+                ?: PropertyTile("Seen", article.seenAt.formatSpanLong())
+            article.accessedAt?.let { PropertyTile("Accessed", it.formatSpanLong()) }
+                ?: PropertyTile("Accessed", "never")
+        }
     }
 }
